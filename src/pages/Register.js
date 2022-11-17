3551918fe3a33b34423828e65a5417a088740dc3
carloshnp <carloshnpwork@gmail.com>
2022-11-16 23:27:34 -0300

    feat: Register page backbone

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,3 +1,60 @@
+import styled from "styled-components";
+import logo from "../img/MyWallet.png";
+
 export default function Register() {
-    return <></>
-};
+  return (
+    <Container>
+      <Logo src={logo} alt="logo" />
+      <Form>
+        <input />
+        <input />
+        <input />
+        <input />
+        <button>Cadastrar</button>
+      </Form>
+      <Link to={`/`}>Já tem uma conta? Entre agora!</Link>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #8c11be;
+`;
+
+const Logo = styled.img`
+  width: 147px;
+  margin-bottom: 24px;
+`;
+
+const Form = styled.form`
+  width: 326px;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    width: 326px;
+    height: 58px;
+    margin-bottom: 13px;
+    box-sizing: border-box;
+    border: 0;
+    border-radius: 5px;
+  }
+
+  button {
+    width: 326px;
+    height: 46px;
+    background-color: #a328d6;
+    border: 0;
+    border-radius: 5px;
+  }
+`;
+
+const Link = styled.h1`
+  margin-top: 36px;
+`;
