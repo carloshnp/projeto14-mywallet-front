e6a0cffcda5b5e5f7c6dbae23da70bb2fa954929
carloshnp <carloshnpwork@gmail.com>
2022-11-21 15:15:55 -0300

    feat: user context added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import GlobalStyle from './GlobalStyle'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Finances from './pages/Finances'
-import Inflow from './pages/Inflow'
-import Outflow from './pages/Outflow'
+import GlobalStyle from "./GlobalStyle";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Finances from "./pages/Finances";
+import Inflow from "./pages/Inflow";
+import Outflow from "./pages/Outflow";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserContextProvider } from "./context/UserContext";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <GlobalStyle />
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/finances" element={<Finances />} />
-                <Route path="/inflow" element={<Inflow /> } />
-                <Route path="/outflow" element={<Outflow />} />
-            </Routes>
-        </BrowserRouter>
-    )
-};
+  return (
+    <UserContextProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/finances" element={<Finances />} />
+          <Route path="/inflow" element={<Inflow />} />
+          <Route path="/outflow" element={<Outflow />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContextProvider>
+  );
+}
